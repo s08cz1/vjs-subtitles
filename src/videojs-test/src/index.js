c7d0ae98c6b4713b1d7dd75affd7e7563725e783
Tom <tomasz.sobkowiak@live.com>
2021-06-10 20:36:30 +0100

    Visual settings amended

--- src/videojs-test/src/index.js
+++ src/videojs-test/src/index.js
@@ -1,10 +1,9 @@
 import 'video.js/dist/video-js.min.css';
 
-
 // libraries
 import videojs from 'video.js';
 
-    // configuration for video.js
+// configuration for video.js
 let options = {
     controls: true,
     bigPlayButton: false,
@@ -13,23 +12,35 @@ let options = {
     fluid: false,
     width: 600,
     height: 300,
+    // textTrackSettings: false // with this set to false, settings can't be set
 };
 let player = videojs('myClip', options, () => {
     // print version information at startup
     let msg = `Using video.js ${videojs.VERSION}`;
     videojs.log(msg);
-
-    // load wav file from url
 });
 
 player.ready(() => {
+    const settings = player.textTrackSettings;
+    settings.setDefaults();
+    settings.setValues({
+        'backgroundColor': '#000',
+        'backgroundOpacity': '1',
+        'edgeStyle': 'none',
+        'fontFamily': 'proportionalSansSerif',
+        'color': '#FFF',
+        'fontPercent': '4.00',
+        'windowColor': '#FF0',
+        'windowOpacity': '0'
+    });
+    settings.updateDisplay();
     const trackEl = player.addRemoteTextTrack({
         src: 'GOPR3467.vtt', 
         kind: 'captions', 
-        label: 'timings', 
+        label: 'Timings 100ms', 
         default: true, 
         language: 'en'
     }, false);
-  });
+ });
 
 player.src({src: 'GOPR3467.MP4', type: 'video/mp4'});
